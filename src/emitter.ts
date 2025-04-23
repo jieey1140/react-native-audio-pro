35a560edc666c55bf67ebe73763796a23bf1ccd6
sarasinakei <seihoonkim@redredgroup.com>
2025-04-23 11:35:47 +0900

    fix: emitter type typo fix

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,11 +1,12 @@
 import { NativeModules, NativeEventEmitter } from 'react-native';
 import { useInternalStore } from './useInternalStore';
 import { logDebug } from './utils';
+import type { AudioProEvent } from './types';
 
 const AudioPro = NativeModules.AudioPro;
 export const emitter = new NativeEventEmitter(AudioPro);
 
-emitter.addListener('AudioProEvent', (event) => {
+emitter.addListener('AudioProEvent', (event: AudioProEvent) => {
 	logDebug('AudioProEvent', JSON.stringify(event));
 	useInternalStore.getState().updateFromEvent(event);
 });
